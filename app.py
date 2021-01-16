0e4f66a55c6e029858828af65d08a745f263386e
djbarnes24601 <70548925+djbarnes24601@users.noreply.github.com>
2021-01-16 14:08:36 -0800

    Added main.css and base.html

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_Template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-    return render_Template('index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
